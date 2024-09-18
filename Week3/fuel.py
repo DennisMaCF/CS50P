eeb17188b741be417113b50ef8865b9cddae4b84
Pochven Mamba <dennisma0423@gmail.com>
2024-09-18 22:57:05 +0800

    Update fuel.py

--- Week3/fuel.py
+++ Week3/fuel.py
@@ -4,6 +4,13 @@ def main():
 def process_ans():
     while True:
         try:
+            #接收用戶輸入之值
+            fraction = input("Fraction: ")
+            #將x/y分成a b c
+            (a, c) = fraction.split("/")
+            (a) = int(a)
+            (c) = int(c)
+            ans = a / c * 100
             if 0 < ans < 100:
                 print(f"{ans:.0f}% ")
             elif ans == 100:
@@ -15,11 +22,3 @@ def process_ans():
             print("請以x/y數入，x,y為整數，y不得為0。 ")
 
 main()
-
-#接收用戶輸入之值
-fraction = input("Fraction: ")
-#將x/y分成a b c
-(a, c) = fraction.split("/")
-(a) = int(a)
-(c) = int(c)
-ans = a / c * 100
